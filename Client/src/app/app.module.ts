52c3d9fd424abfe2213db54814f3e619be17569f
Stephen <stephen.goedhart@swisscom.com>
2022-07-24 13:33:45 +0200

    Added 3 panel layout

--- Client/src/app/app.module.ts
+++ Client/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LeftPaneComponent } from './content/left-pane/left-pane.component';
+import { ContentComponent } from './content/content.component';
+import { CalendarComponent } from './content/calendar/calendar.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, ContentComponent, LeftPaneComponent, CalendarComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
